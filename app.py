f55000aadaf0ca628e5df7afc237cb0bd91255a3
JakeChow <56766171+JakeChow@users.noreply.github.com>
2019-10-20 07:59:47 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, send_from_directory, render_template, request
-from getData import *
+import getData
+import GetCoord
+
+
 app = Flask(__name__, static_url_path='')
+a = '/'
+windSpeed = getData.readJsonDump('windSpeedDatabase')
+windDirection = getData.readJsonDump('windDirectionDatabase')
+solarIrradiance = getData.readJsonDump('solarIrradianceDatabase')
 
 @app.route('/')
 def my_form():
@@ -11,9 +18,22 @@ def parse_data():
     if request.method == "POST":
     	x = request.form['textbox']
     	print(x)
-    	if x != "":
-    		return render_template('home.html', location=x)
-
+        coords = GetCoord.locate(x)
+    	la = coords.split(':')[0]
+    	lo = coords.split(':')[1]
+        coords = lo + ":" + la
+        try:
+    	   key = getData.convertToValidKey(float(la), float(lo))
+    	   windSpeedAverage = getData.getTotalAverage(windSpeed, key)
+    	   windDirectionAverage = getData.getTotalAverage(windDirection, key)
+    	   solarIrradianceAverage = getData.getTotalAverage(solarIrradiance, key)
+    	
+           januaryWindSpeed = getData.getMonthlyAverage(windSpeed, key, "1")
+    	   return render_template('home.html', location=coords, 
+    		  speed=windSpeedAverage, direction=windDirectionAverage, irradiance=solarIrradianceAverage, january=januaryWindSpeed)
+        except:
+            return render_template('home.html', location=coords, 
+              speed="", direction="", irradiance="", january="")
 @app.route('/test')
 def runExternalScript():
 	x = getData('windDirectionDatabase')
---
+++ predict.py
@@ -0,0 +1,37 @@
+
+## SOLAR AVERAGES ##
+SolarUnits = "kW-hr/m^2/day"
+AverageResidentialWattCost = 2.99
+
+# ALL IN KILOWATT HOURS
+AverageResidentialKiloWattCost = 2990
+AverageDailyHouseholdProduction = 5
+AverageMonthlyHouseholdProduction = 909
+AverageYearlyHouseholdProduction = 10909
+AverageDailyHouseholdConsumption = 30.3
+AveragePanelDailyWattageProduction = 320
+AveragePanelPrice = 960.00
+AveragePanelCount = 15.63
+AverageTotalCost = 14400.00
+AveragePanelSize = 17.55
+
+
+## WIND AVERAGES ##
+WindUnits = "kW-hr/m^2/day"
+WindSpeedUnits = "m/s"
+AverageCommercialWindWattPrice = 1700000
+AverageCommercialTurbinePrice = 3500000
+AverageResidentialMaximumOutput = 36
+AverageResidentialMinimumPrice = 20000
+AverageResidentialMaximumPrice = 100000
+
+#m/s
+AverageResidentialEconomicSpeed = 7
+AverageResidentialStartupSpeed = 2
+AverageResidentialGenerationSpeed = 3.5
+AverageResidentialOptimalSpeed = 15
+AverageResidentialBreakingSpeed = 25
+AverageResidentialMinimumOutput = 2
+
+
+def weatherComparison(a, b, c):
